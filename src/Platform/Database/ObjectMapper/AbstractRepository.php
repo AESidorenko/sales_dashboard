2fbb07336e13789595e13d65e4a26adae244ebdb
Anton <aesidorenko@yandex.ru>
2020-12-24 04:21:18 +0300

    Implement mysqli_fetch iterator

--- src/Platform/Database/ObjectMapper/AbstractRepository.php
+++ src/Platform/Database/ObjectMapper/AbstractRepository.php
@@ -3,7 +3,6 @@
 namespace App\Platform\Database\ObjectMapper;
 
 use App\Platform\Database\DatabaseConnectionInterface;
-use EmptyIterator;
 use Iterator;
 
 abstract class AbstractRepository
@@ -14,49 +13,10 @@ abstract class AbstractRepository
 
     public function __construct(DatabaseConnectionInterface $connection, string $entityClassname)
     {
-        self::$connection      = $connection;
+        self::$connection = $connection;
+
         $this->entityClassname = $entityClassname;
         $this->tablename       = $entityClassname::getTableName();
-    }
-
-    public function findOne(string $id): ?EntityInterface
-    {
-        $result = $this->query("SELECT * FROM `%s` WHERE id=%s", [$this->tablename, $id]);
-        if (iterator_count($result) === 0) {
-            return null;
-        }
-
-        return $result->current();
-    }
-
-    public function findOneBy(string $condition): ?EntityInterface
-    {
-        $result = $this->query("SELECT * FROM `%s` WHERE %s", [$this->tablename, $condition]);
-        if (iterator_count($result) === 0) {
-            return null;
-        }
-
-        return $result->current();
-    }
-
-    public function findBy(string $condition = 'true'): Iterator
-    {
-        $result = $this->query("SELECT * FROM `%s` WHERE %s", [$this->tablename, $condition]);
-        if (iterator_count($result) === 0) {
-            return new EmptyIterator();
-        }
-
-        return $result;
-    }
-
-    public function findAll(): Iterator
-    {
-        $result = $this->query("SELECT * FROM `%s`", [$this->tablename]);
-        if (iterator_count($result) === 0) {
-            return new EmptyIterator();
-        }
-
-        return $result;
     }
 
     protected function query(string $sql, array $params, bool $map = true): Iterator
